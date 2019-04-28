13f2ea55cd3b140aeeb0e59ec3f9637ab3efd9e6
jordi007 <jordi01carcamo@gmail.com>
2019-04-28 01:03:07 -0600

    parte del modelo

--- node_crud/models/contacto.js
+++ node_crud/models/contacto.js
@@ -4,9 +4,32 @@ let Schema = mongoose.Schema;
 
 let schema = new Schema({
     id: { type: String },
-    nombres: { type: String },
     apellidos: { type: String },
-    edad: { type: Number, min: 0 }
+    identidad: { type: String },
+    rtn: { type: String },
+    edad: { type: String},
+    direcciones: [
+        {
+            id: { type: String},
+            descripcion: { type: String},
+            tipo: { type: String}
+        }
+    ],
+    telefonos: [
+        {
+            numero: String,
+            direccione: {type: String}, 
+        }
+    ],
+    tipo: { type: String },
+    fechas: [
+        {
+            fecha: { type: String},
+            descripcion: { type: String}
+        }
+    ],
+    contactosAsociados: [],
+    tipoUsuario: { type: String} 
 }, { versionKey: false });
 
 let Contacto = mongoose.model('contactos', schema);
